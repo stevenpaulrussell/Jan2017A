9ad89547591112e812798d76c0bb393492eddb33
steve <steven_russell@hotmail.com>
2017-01-09 18:20:50 -0800

    Passing tests to read, write simple yaml file.

--- read_directory.py
+++ read_directory.py
@@ -1,6 +1,24 @@
-from yaml import reader, dumper
+import yaml
+import os
+
+
 
 
 
 def read_directory(loc='/Users/steve/jan2017A'):
     return {'db_spec': None, 'open_spreadsheets': None, 'mirrored_spreadsheets': None}
+
+
+def read_yaml(yaml_path):
+    with open(yaml_path,'r') as fp:
+        data = yaml.safe_load(fp)
+    return data
+
+
+def write_yaml(data, yaml_path):
+    yaml.safe_dump(data, yaml_path)
+
+
+
+
+
--- test_severalitems.py
+++ test_severalitems.py
@@ -1,6 +1,27 @@
 import unittest
+import os
+
 
 import read_directory
+
+
+local_path = '/Users/steve/'
+test_yaml_filename = 'test_yaml_file'
+yaml_test_dictionary = {'one': 1, 'two': 2}
+
+
+class Test_yaml_functions(unittest.TestCase):
+  def setUp(self):
+    self.test_yaml_path = os.path.join(local_path, test_yaml_filename)
+    with open(self.test_yaml_path,'w') as fp:
+        read_directory.write_yaml(yaml_test_dictionary, fp)
+
+  def tearDown(self):
+    os.remove(self.test_yaml_path)
+
+  def test_did_yaml_correctly(self):
+    test_data = read_directory.read_yaml(self.test_yaml_path)
+    self.assertEqual(test_data, yaml_test_dictionary)
 
 
 class Test_read_directory(unittest.TestCase):
@@ -22,8 +43,6 @@ class Test_read_db_spec(unittest.TestCase):
     mydirectory = read_directory.read_directory()
     self.db_spec_path = mydirectory['db_spec']
 
-  def test_can_import_db_spec(self):
-    self.assertFalse('Finish the test')
 
 
 
