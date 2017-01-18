57ca1c9a51c21c86098673139bfc513a997b2355
steve <steven_russell@hotmail.com>
2017-01-18 13:55:10 -0800

    Another intermediate commit

--- sentry.py
+++ sentry.py
@@ -2,25 +2,39 @@ import os
 import time
 import json
 
-import file_utilities
 
-file_stats = dict(default='should not be seen')
+previous_file_data = {}
 
-def get_file_stats(apath):
-    paths, dirs, files = os.walk(apath).__next__()
-    return dirs, files
+
+def take_roll_of_new_changes_and_missing(apath):
+    global previous_file_data, current_file_data
+    previous_file_names = set(previous_file_data.keys())
+    current_file_data = get_current_file_stats(apath)
+    current_file_names = set(current_file_data.keys())
+    new = current_file_names - previous_file_names
+    missing = previous_file_names - current_file_names
+    persisted = current_file_names.intersection(previous_file_names)
+    changed = set()
+    for file_name in persisted:
+        if current_file_data[file_name] != previous_file_data[file_name]:
+            changed.add(file_name)
+    return new, missing, changed
+
+
+def get_current_file_stats(apath):
+    current_file_data = {}
+    for file_name in os.walk(apath).__next__()[-1]:
+        current_file_data[file_name] = os.stat(os.path.join(apath, file_name)).st_mtime
+    return current_file_data
 
 
 def load_json_from_dot_sentry(apath):
-    global file_stats
-    apath = os.path.join(apath, '.sentry')
-    try:
-        with open(apath, 'r') as fp:
-            file_stats = json.load(fp)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        file_stats = {}
+    global previous_file_data
+    with open(os.path.join(apath, '.sentry'), 'r') as fp:
+        previous_file_data = json.load(fp)
 
 
-def dump_json_to_dot_sentry():
-    with open(apath, 'w') as fp:
-        json.dump(file_stats, fp)
+def dump_json_to_dot_sentry(apath):
+    global previous_file_data
+    with open(os.path.join(apath, '.sentry'), 'w') as fp:
+        json.dump(previous_file_data, fp)
--- test_sentry.py
+++ test_sentry.py
@@ -48,10 +48,10 @@ class TestSentryGetsFileChanges(unittest.TestCase):
         self.temp_path = os.path.join(dropbox_directory_path, temp_name)
 
     def tearDown(self):
-        try:
-            os.remove(self.temp_path)
-        except FileNotFoundError:
-            pass
+        file_names = next(os.walk(dropbox_directory_path))[-1]
+        for file_name in file_names:
+            os.remove(os.path.join(dropbox_directory_path, file_name))
+
 
     def make_a_file(self, file_name=temp_name):
         path = os.path.join(dropbox_directory_path, file_name)
@@ -61,19 +61,34 @@ class TestSentryGetsFileChanges(unittest.TestCase):
     def test_can_see_files_and_dirs_in_sentry(self):
         self.make_a_file()
         self.make_a_file('.sentry')
-        dirs, files = sentry.get_file_stats(dropbox_directory_path)
-        self.assertEqual(sentry.file_stats, {'default': 'should not be seen'})
-        self.assertEqual(dirs, [])
-        self.assertIn(temp_name, files)
-        self.assertIn('.sentry', files)
+        file_dictionary = sentry.get_current_file_stats(dropbox_directory_path)
+        self.assertEqual(sentry.previous_file_data, {})
+        self.assertIn(temp_name, file_dictionary)
+        self.assertIn('.sentry', file_dictionary)
 
     def test_load_json_from_dot_sentry_returns_None_if_no_dot_sentry(self):
+        sentry.dump_json_to_dot_sentry(dropbox_directory_path)
         sentry.load_json_from_dot_sentry(dropbox_directory_path)
-        self.assertEqual(sentry.file_stats, {})
+        self.assertEqual(sentry.previous_file_data, {})
 
     def test_dump_json_from_dot_sentry_returns_None_if_no_dot_sentry(self):
+        sentry.dump_json_to_dot_sentry(dropbox_directory_path)
         sentry.load_json_from_dot_sentry(dropbox_directory_path)
-        self.assertEqual(sentry.file_stats, {})
+        self.assertEqual(sentry.previous_file_data, {})
+
+    def test_sentry_take_roll_of_new_changes_and_missing_finds_nothing_with_nothing_in(self):
+        new, changed, missing = sentry.take_roll_of_new_changes_and_missing(dropbox_directory_path)
+        self.assertEqual(new, set())
+        self.assertEqual(missing, set())
+
+    def test_sentry_take_roll_of_new_changes_and_missing_finds_two_new_files(self):
+        self.make_a_file()
+        self.make_a_file('.sentry')
+        new, changed, missing = sentry.take_roll_of_new_changes_and_missing(dropbox_directory_path)
+        self.assertIn('temp_name', new)
+        self.assertIn('.sentry', new)
+        self.assertEqual(changed, set())
+        self.assertEqual(missing, set())
 
 if __name__ == '__main__':
     unittest.main()
