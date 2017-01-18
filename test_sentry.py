6fcfb5c4c2d68fa9e8b3b7848b8f45005d9f9796
steve <steven_russell@hotmail.com>
2017-01-18 11:24:38 -0800

    Wrote tests to explore os.stat functions prior to coding anything in sentry

--- test_sentry.py
+++ test_sentry.py
@@ -1,14 +1,42 @@
+import os
+import time
 import unittest
 
 import setup_common_for_test
 
 
+dropbox_directory_info = setup_common_for_test.test_directory['dropbox_test']
+
 
 class SmokeAndLearningiTestCase(unittest.TestCase):
-    def test_can_see_folders_in_dropbox(self):
+    def setUp(self):
+        self.assertTrue(os.path.exists(dropbox_directory_info['path']))
+        self.temp_name = 'temp_name'
+        self.temp_path = os.path.join(dropbox_directory_info['path'], self.temp_name)
+        if os.path.exists(self.temp_path):
+            os.remove(self.temp_path)
 
-        self.assertEqual(True, False)
+    def tearDown(self):
+        os.remove(self.temp_path)
 
+    def make_a_file(self):
+        with open(self.temp_path, 'w') as fp:
+            fp.write('hello\n')
+
+    def test_can_create_files_in_dropbox_and_get_info(self):
+        self.make_a_file()
+        self.assertTrue(os.path.exists(self.temp_path))
+
+    def test_can_get_file_stats(self):
+        self.make_a_file()
+        made = os.stat(self.temp_path).st_mtime
+        time.sleep(2)
+        with open(self.temp_path, 'a') as fp:
+            fp.write('goodbye\n')
+        changed = os.stat(self.temp_path).st_mtime
+        lapsed = changed - made  # Python os.stat docs say that .st_mtime has a two second granularity
+        self.assertTrue(lapsed > 1)
+        self.assertTrue(lapsed < 3)
 
 if __name__ == '__main__':
     unittest.main()
