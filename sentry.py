44680b19afd53cf542aee7b33bd73c1dc6be145f
steve <steven_russell@hotmail.com>
2017-01-18 12:48:18 -0800

    Intermediate commit of sentry and test

--- sentry.py
+++ sentry.py
@@ -1,6 +1,26 @@
 import os
+import time
+import json
 
 import file_utilities
 
+file_stats = dict(default='should not be seen')
+
+def get_file_stats(apath):
+    paths, dirs, files = os.walk(apath).__next__()
+    return dirs, files
 
 
+def load_json_from_dot_sentry(apath):
+    global file_stats
+    apath = os.path.join(apath, '.sentry')
+    try:
+        with open(apath, 'r') as fp:
+            file_stats = json.load(fp)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        file_stats = {}
+
+
+def dump_json_to_dot_sentry():
+    with open(apath, 'w') as fp:
+        json.dump(file_stats, fp)
--- test_sentry.py
+++ test_sentry.py
@@ -3,24 +3,27 @@ import time
 import unittest
 
 import setup_common_for_test
+import sentry
 
 
-dropbox_directory_info = setup_common_for_test.test_directory['dropbox_test']
-
+dropbox_directory_path = setup_common_for_test.test_directory['dropbox_test']['path']
+temp_name = 'temp_name'
 
 class SmokeAndLearningiTestCase(unittest.TestCase):
     def setUp(self):
-        self.assertTrue(os.path.exists(dropbox_directory_info['path']))
-        self.temp_name = 'temp_name'
-        self.temp_path = os.path.join(dropbox_directory_info['path'], self.temp_name)
-        if os.path.exists(self.temp_path):
-            os.remove(self.temp_path)
+        self.assertTrue(os.path.exists(dropbox_directory_path))
+        self.temp_path = os.path.join(dropbox_directory_path, temp_name)
 
     def tearDown(self):
-        os.remove(self.temp_path)
+        try:
+            os.remove(self.temp_path)
+        except FileNotFoundError:
+            pass
 
-    def make_a_file(self):
-        with open(self.temp_path, 'w') as fp:
+
+    def make_a_file(self, file_name=temp_name):
+        path = os.path.join(dropbox_directory_path, file_name)
+        with open(path, 'w') as fp:
             fp.write('hello\n')
 
     def test_can_create_files_in_dropbox_and_get_info(self):
@@ -37,6 +40,40 @@ class SmokeAndLearningiTestCase(unittest.TestCase):
         lapsed = changed - made  # Python os.stat docs say that .st_mtime has a two second granularity
         self.assertTrue(lapsed > 1)
         self.assertTrue(lapsed < 3)
+
+
+class TestSentryGetsFileChanges(unittest.TestCase):
+    def setUp(self):
+        self.assertTrue(os.path.exists(dropbox_directory_path))
+        self.temp_path = os.path.join(dropbox_directory_path, temp_name)
+
+    def tearDown(self):
+        try:
+            os.remove(self.temp_path)
+        except FileNotFoundError:
+            pass
+
+    def make_a_file(self, file_name=temp_name):
+        path = os.path.join(dropbox_directory_path, file_name)
+        with open(path, 'w') as fp:
+            fp.write('hello\n')
+
+    def test_can_see_files_and_dirs_in_sentry(self):
+        self.make_a_file()
+        self.make_a_file('.sentry')
+        dirs, files = sentry.get_file_stats(dropbox_directory_path)
+        self.assertEqual(sentry.file_stats, {'default': 'should not be seen'})
+        self.assertEqual(dirs, [])
+        self.assertIn(temp_name, files)
+        self.assertIn('.sentry', files)
+
+    def test_load_json_from_dot_sentry_returns_None_if_no_dot_sentry(self):
+        sentry.load_json_from_dot_sentry(dropbox_directory_path)
+        self.assertEqual(sentry.file_stats, {})
+
+    def test_dump_json_from_dot_sentry_returns_None_if_no_dot_sentry(self):
+        sentry.load_json_from_dot_sentry(dropbox_directory_path)
+        self.assertEqual(sentry.file_stats, {})
 
 if __name__ == '__main__':
     unittest.main()
