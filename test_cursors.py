981c189681a6963d0d2083dd594c4e4c8eebb594
steve <steven_russell@hotmail.com>
2017-01-17 18:45:40 -0800

    Refactor tests

--- test_cursors.py
+++ test_cursors.py
@@ -15,23 +15,21 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(dataqueda_constants.LOCAL_CONNECT)
         self.assertTrue(cmdr.tableset)
 
-    def test_aws_connects_and_sees_no_tables(self):
-        cmdr = cursors.Commander(False)
-        self.assertFalse(cmdr.tableset)
 
-    def test_aws_connect_gets_exception_on_nonsense(self):
-        cmdr = cursors.Commander(connect_to_local_database=False)
-        exception_msg = cmdr.do_cmd('SELECT *')
-        self.assertTrue(exception_msg)
+class CanConnectRemotelyAndMakeTables(unittest.TestCase):
+    def setUp(self):
+        self.cmdr = cursors.Commander(connect_to_local_database=False)
 
+    def tearDown(self):
+        self.cmdr.close()
 
-class CanMakeTables(unittest.TestCase):
+    def test_remote_has_no_tables_to_start(self):
+        self.assertFalse(self.cmdr.tableset)
+
     def test_one_table_making_remote(self):
-        cmdr = cursors.Commander(connect_to_local_database=False)
-        self.assertFalse(cmdr.tableset)
-        cmdr.do_cmd(sql_command_library.read_db_creation_commands(test_directory)['person'])
-        self.assertTrue(cmdr.tableset)
-        self.assertIn('person', cmdr.tableset)
+        self.cmdr.do_cmd(sql_command_library.read_db_creation_commands(test_directory)['person'])
+        self.assertTrue(self.cmdr.tableset)
+        self.assertIn('person', self.cmdr.tableset)
 
 
 
