d62e0715b34674a948125d193f3e16754c9ac956
steve <steven_russell@hotmail.com>
2017-01-22 17:34:15 -0800

    Branch has a better way to read, write create, view, query commands.  Need more test

--- sql_command_library.py
+++ sql_command_library.py
@@ -1,4 +1,5 @@
 import collections
+import os
 import yaml
 
 import file_utilities
@@ -6,50 +7,58 @@ import file_utilities
 import db_cmd_makers, db_view_makers, db_query_makers
 
 
-
 def read_db_creation_commands(my_directory):
-    db_creation_commands = collections.OrderedDict()
-    with open(my_directory['create_table_cmds'], 'r') as fp:
-        commands_as_string = fp.read()
-    commands_as_list = commands_as_string.split('\n\n')
-    for item in commands_as_list:
-        firstline = item.split('\n')[0]
-        tablename = firstline.split(' TABLE ')[-1].strip()
-        db_creation_commands[tablename] = item
-    return db_creation_commands
+    return read_cmds_as_list(my_directory['create_table_cmds'])
 
 
 def read_cmds_as_list(my_path):
+    wanted = collections.OrderedDict()
     with open(my_path, 'r') as fp:
         commands_as_string = fp.read()
     commands_as_list = commands_as_string.split('\n\n')
-
-
+    for item in commands_as_list:
+        lines = item.split('\n')
+        item_name, item = lines[0], '\n'.join(lines[1:])
+        wanted[item_name] = item
+    return wanted
 
 
 def write_db_creation_commands(my_directory):
     template_line_gen = file_utilities.spreadsheet_keyvalue_generator(my_directory['db_template'])
     db_commands = db_cmd_makers.extract_sql_table_cmds(template_line_gen)
-    write_cmds(my_directory['create_table_cmds'], [table.createtable_cmdstring for table in db_commands.values()])
-    write_cmds(my_directory['insert_into_table_cmds'], [table.insert_cmdstring for table in db_commands.values()])
-    write_cmds(my_directory['table_as_query_cmds'], [table.create_query_cmd_string for table in db_commands.values()])
+    create_path = clean_and_return_path(my_directory, 'create_table_cmds')
+    insert_path = clean_and_return_path(my_directory, 'insert_into_table_cmds')
+    query_path = clean_and_return_path(my_directory, 'table_as_query_cmds')
+    for table_name, table in db_commands.items():
+        write_cmd_string(table_name, create_path, table.createtable_cmdstring)
+        write_cmd_string(table_name, insert_path, table.insert_cmdstring)
+        write_cmd_string(table_name, query_path, table.create_query_cmd_string)
 
 
 def write_view_creation_commands(my_directory):
     template_line_gen = file_utilities.spreadsheet_keyvalue_generator(my_directory['view_template'])
     view_commands = db_view_makers.extract_sql_view_cmds(template_line_gen)
-    write_cmds(my_directory['create_view_cmds'], [view.create_view_cmd_string for view in view_commands.values()])
-    write_cmds(my_directory['view_as_query_cmds'], [view.create_query_cmd_string for view in view_commands.values()])
+    create_path = clean_and_return_path(my_directory, 'create_view_cmds')
+    query_path = clean_and_return_path(my_directory, 'view_as_query_cmds')
+    for view_name, view in view_commands.items():
+        write_cmd_string(view_name, create_path, view.create_view_cmd_string)
+        write_cmd_string(view_name, query_path, view.create_query_cmd_string)
 
 
 def write_query_creation_commands(my_directory):
     template_line_gen = file_utilities.spreadsheet_keyvalue_generator(my_directory['query_template'])
     query_commands = db_query_makers.extract_sql_query_cmds(template_line_gen)
-    write_cmds(my_directory['create_query_cmds'], [query.create_query_cmd_string for query in query_commands.values()])
+    query_path = clean_and_return_path(my_directory, 'create_query_cmds')
+    for query_name, query in query_commands.items():
+        write_cmd_string(query_name, query_path, query.create_query_cmd_string)
 
 
-def write_cmds(my_path, my_gen):
-    with open(my_path, 'w') as fp:
-        for item in my_gen:
-            fp.write(item + '\n\n')
+def write_cmd_string(item_name, my_path, command_string):
+    with open(my_path, 'a') as fp:
+        fp.write('{}\n{}\n\n'.format(item_name, command_string))
 
+def clean_and_return_path(my_directory, alias):
+    my_path = my_directory[alias]
+    if os.path.exists(my_path):
+        os.remove(my_path)
+    return my_path
