ab9d2022d650b77373f729d511755b37c5cdb3de
steve <steven_russell@hotmail.com>
2017-01-23 18:50:48 -0800

    Beginning to work on action, tying all together, using both functional test and unit test of action.  Need to worry about the directories, both for test and for operation.

---
+++ functional_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class MyTestCaseDrivesFirstRunThroughSimply(unittest.TestCase):
+    """
+    Adding a spreadsheet to a directory results in an action detecting the addition, finding the right table for
+    data insertion, finding the conditions to commit the insertion and what to do about 'mirroring', doing the
+    insertion (presume no errors for now), and reporting the results.
+
+    Need to specify the directories, how they chain, how they instruct
+    
+    """
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
