3f6b1af57e3a1271e6fa1dca891cc9852d5b0752
steve <steven_russell@hotmail.com>
2017-01-26 18:56:20 -0800

    Added some notes to myself

--- action.py
+++ action.py
@@ -10,4 +10,9 @@ def poll_imports(imports_path):
         new, changed, missing = sentry.take_roll_of_new_changes_and_missing(path)
         if any((new, changed, missing)):
             work_list.append((table, to_do, path, new, changed, missing))
+            # Need to check for legal state given changes and value of to_do, take account of missing if file moved
+            # With those changes, could do the insert into table.
+            # But want to be careful with sql exception handling and making error file.
+            # Need some test harness including initialization and moving files! Must think about test_directory vs
+            # actual operations.
     return work_list
