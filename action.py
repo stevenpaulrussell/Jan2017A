49ecea0b4edb0ec1c4aee99509e1fc187f8f9801
steve <steven_russell@hotmail.com>
2017-01-26 18:53:15 -0800

    Passing tests putting file in, taking it out of person.

--- action.py
+++ action.py
@@ -3,12 +3,11 @@ import sentry
 
 
 def poll_imports(imports_path):
+    work_list = []
     imports_gen = file_utilities.spreadsheet_keyvalue_generator(imports_path)
     for location in imports_gen:
         table, to_do, path = location['table'], location['action'], location['path']
         new, changed, missing = sentry.take_roll_of_new_changes_and_missing(path)
-    return [(table, to_do, path), (new, changed, missing)]
-
-
-def look_for_changes(a_path):
-    new, changed, missing = sentry.take_roll_of_new_changes_and_missing()
+        if any((new, changed, missing)):
+            work_list.append((table, to_do, path, new, changed, missing))
+    return work_list
--- test_action.py
+++ test_action.py
@@ -11,11 +11,17 @@ imports_path = test_directory['imports_locator']
 
 class MyTestCase(unittest.TestCase):
     def test_action_polls_all_directories_for_changes(self):
-        (table, to_do, path), (new, changed, missing) = action.poll_imports(test_directory['imports_locator'])
-        self.assertEqual(table, 'person')
-        self.assertEqual(to_do, 'import whole')
-        self.assertTrue(os.path.exists(path))
-        print(new, changed, missing)
+        work_list = action.poll_imports(test_directory['imports_locator'])
+        if work_list:
+            for work_item in work_list:
+                (table, to_do, path, new, changed, missing) = work_item
+                self.assertEqual(table, 'person')
+                self.assertEqual(to_do, 'import whole')
+                self.assertTrue(os.path.exists(path))
+                print(table, path, new, changed, missing)
+        else:
+            print('No change')
+            self.assertEqual(work_list, [])
 
 
 if __name__ == '__main__':
