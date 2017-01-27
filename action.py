5ef566522bfc4b6240826c548a924fd0d87ceb75
steve <steven_russell@hotmail.com>
2017-01-27 09:44:17 -0800

    Storing work items in a global list, each work item is now an object holding state

--- action.py
+++ action.py
@@ -1,18 +1,41 @@
+import os
+
 import file_utilities
 import sentry
 
+work_list = []
 
 def poll_imports(imports_path):
-    work_list = []
     imports_gen = file_utilities.spreadsheet_keyvalue_generator(imports_path)
     for location in imports_gen:
-        table, to_do, path = location['table'], location['action'], location['path']
+        table_name, to_do, path = location['table'], location['action'], location['path']
         new, changed, missing = sentry.take_roll_of_new_changes_and_missing(path)
+        if new:
+            for file_name in new:
+                WorkItem(table_name, to_do, path, file_name, 'new')
+        if changed:
+            for file_name in changed:
+                WorkItem(table_name, to_do, path, file_name, 'changed')
+        if missing:
+            for file_name in missing:
+                WorkItem(table_name, to_do, path, file_name, 'missing')
         if any((new, changed, missing)):
-            work_list.append((table, to_do, path, new, changed, missing))
-            # Need to check for legal state given changes and value of to_do, take account of missing if file moved
-            # With those changes, could do the insert into table.
-            # But want to be careful with sql exception handling and making error file.
-            # Need some test harness including initialization and moving files! Must think about test_directory vs
-            # actual operations.
-    return work_list
+            break
+
+        # Need to check for legal state given changes and value of to_do, take account of missing if file moved
+        # With those changes, could do the insert into table.
+        # But want to be careful with sql exception handling and making error file.
+        # Need some test harness including initialization and moving files! Must think about test_directory vs
+        # actual operations.
+
+
+class WorkItem(object):
+    def __init__(self, table_name, to_do, directory, file_name, file_change):
+        self.table_name = table_name
+        self.to_do = to_do
+        self.directory = directory
+        self.file_name = file_name
+        self.path = os.path.join(directory, file_name)
+        self.file_change = file_change
+        work_list.append(self)
+
--- test_action.py
+++ test_action.py
@@ -11,17 +11,17 @@ imports_path = test_directory['imports_locator']
 
 class MyTestCase(unittest.TestCase):
     def test_action_polls_all_directories_for_changes(self):
-        work_list = action.poll_imports(test_directory['imports_locator'])
-        if work_list:
-            for work_item in work_list:
-                (table, to_do, path, new, changed, missing) = work_item
-                self.assertEqual(table, 'person')
-                self.assertEqual(to_do, 'import whole')
-                self.assertTrue(os.path.exists(path))
-                print(table, path, new, changed, missing)
+        action.poll_imports(test_directory['imports_locator'])
+        if action.work_list:
+            print('Seeing {} work items'.format(len(action.work_list)))
+            for work_item in action.work_list:
+                self.assertEqual(work_item.table_name, 'person')
+                self.assertEqual(work_item.to_do, 'import whole')
+                self.assertIn(work_item.file_change, ('new', 'missing'))
+                if work_item.file_change == 'new':
+                    self.assertTrue(os.path.exists(work_item.path))
         else:
             print('No change')
-            self.assertEqual(work_list, [])
 
 
 if __name__ == '__main__':
