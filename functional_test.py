4997b91951c6eb4344e96fbc8b0447100f24499c
steve <steven_russell@hotmail.com>
2017-01-26 17:20:31 -0800

    Added to user story

--- functional_test.py
+++ functional_test.py
@@ -7,10 +7,21 @@ class MyTestCaseDrivesFirstRunThroughSimply(unittest.TestCase):
     data insertion, finding the conditions to commit the insertion and what to do about 'mirroring', doing the
     insertion (presume no errors for now), and reporting the results.
 
+    Putting a file into the new_imports_whole/person folder results in that file being imported into the person table
+    if there are no errors.  If no errors were found, the file is moved from imports_whole/person to mirrors/person.
+    If there are errors, the file is removed from person and put into error_files with the errors shown per line.
+
     Need to specify the directories, how they chain, how they instruct
+
+
 
     """
     def test_something(self):
+        # move a test file into new_imports_whole/person
+        # call action to poll for changes
+        # call action to run the found change
+        # have different tests for case file has errors, has no errors.
+        # don't forget to initialize and clean up.
         self.assertEqual(True, False)
 
 
