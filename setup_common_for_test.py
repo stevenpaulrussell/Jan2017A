f22ffbc58e7ee21fb2804521ca32f481de824e79
steve <steven_russell@hotmail.com>
2017-01-27 12:08:31 -0800

    Refactoring:  ... moving items from test to setup common, renaming

--- setup_common_for_test.py
+++ setup_common_for_test.py
@@ -6,6 +6,22 @@ test_source_path = '/Users/steve/TestItemsForJan2017A'
 test_yaml_path = os.path.join(test_source_path, 'test_yaml_file')
 test_directory_path = os.path.join(test_source_path, 'test_directory.xlsx')
 
+#Following hardcoded here instead of in test_directory.  Reason is that these are used to test functions needed for
+# test_directory to be read
+
+test_bad_header_spreadsheet_path = os.path.join(test_source_path, 'bad_header_spreadsheet_test.xls')
+test_header_only_spreadsheet_path = os.path.join(test_source_path, 'header_only_spreadsheet_test.xls')
+test_various_problems_spreadsheets_path = os.path.join(test_source_path, 'various_problems_spreadsheet_test.xls')
+test_empty_spreadsheet_path = os.path.join(test_source_path, 'empty_spreadsheet_test.xls')
+
+
+assert os.path.exists(test_source_path)
+assert os.path.exists(test_bad_header_spreadsheet_path)
+assert os.path.exists(test_header_only_spreadsheet_path)
+assert os.path.exists(test_various_problems_spreadsheets_path)
+assert os.path.exists(test_empty_spreadsheet_path)
+
+
 yaml_test_dictionary = {'test_directory': test_directory_path}
 test_directory = file_utilities.read_my_directory(yaml_test_dictionary['test_directory'])
 
@@ -16,3 +32,5 @@ def read_test_locations():
         test_locations[alias] = os.path.join(info['path'], info['filename'])
     return test_locations
 
+
+print('***** setup_common_for_test is loaded, checks run *****')
--- test_file_utilities.py
+++ test_file_utilities.py
@@ -2,52 +2,24 @@ import unittest
 import os
 from itertools import chain
 
-from setup_common_for_test import test_source_path
-from setup_common_for_test import test_yaml_path
-from setup_common_for_test import test_directory_path
-from setup_common_for_test import read_test_locations
-from setup_common_for_test import yaml_test_dictionary
-
-import setup_common_for_test
-
 import file_utilities
-
-test_bad_header_spreadsheet_path = os.path.join(test_source_path, 'bad_header_spreadsheet_test.xls')
-test_header_only_spreadsheet_path = os.path.join(test_source_path, 'header_only_spreadsheet_test.xls')
-test_various_problems_spreadsheets_path = os.path.join(test_source_path, 'various_problems_spreadsheet_test.xls')
-test_empty_spreadsheet_path = os.path.join(test_source_path, 'empty_spreadsheet_test.xls')
-
-
-class Test_Test_Readiness(unittest.TestCase):
-    def test_test_source_path(self):
-        self.assertTrue(os.path.exists(test_source_path))
-        self.assertTrue(os.path.exists(test_bad_header_spreadsheet_path))
-        self.assertTrue(os.path.exists(test_header_only_spreadsheet_path))
-        self.assertTrue(os.path.exists(test_various_problems_spreadsheets_path))
-        self.assertTrue(os.path.exists(test_empty_spreadsheet_path))
+import setup_common_for_test as common
 
 
 class Test_Do_Setups(unittest.TestCase):
     """Super class for tests in order to not duplicate setup and teardown functions"""
     def setUp(self):
-        with open(test_yaml_path, 'w') as fp:
-            file_utilities.write_yaml(yaml_test_dictionary, fp)
+        with open(common.test_yaml_path, 'w') as fp:
+            file_utilities.write_yaml(common.yaml_test_dictionary, fp)
 
     def tearDown(self):
-        os.remove(test_yaml_path)
-
-
-class Test_Yaml_Functions(Test_Do_Setups):
-    def test_did_yaml_correctly(self):
-        self.assertTrue(os.path.exists(test_yaml_path))
-        test_data = file_utilities.read_yaml(test_yaml_path)
-        self.assertEqual(test_data, yaml_test_dictionary)
+        os.remove(common.test_yaml_path)
 
 
 class Test_get_directories_and_db_spec_from_yaml(Test_Do_Setups):
     def setUp(self):
         super().setUp()
-        self.mydirectory = file_utilities.read_yaml(test_yaml_path)
+        self.mydirectory = file_utilities.read_yaml(common.test_yaml_path)
 
     def test_can_get_perm_spreadsheets_directory(self):
         perm_spreadsheets_directory_path = self.mydirectory['test_directory']
@@ -56,7 +28,7 @@ class Test_get_directories_and_db_spec_from_yaml(Test_Do_Setups):
 
 class Test_can_read_a_spreadsheet(Test_Do_Setups):
     def test_various_problems_sheet(self):
-        data_generator = file_utilities.spreadsheet_keyvalue_generator(test_various_problems_spreadsheets_path)
+        data_generator = file_utilities.spreadsheet_keyvalue_generator(common.test_various_problems_spreadsheets_path)
         first_line = data_generator.__next__()
         self.assertEqual(first_line['spaces_in_middle_and_end'], 'the header    and this have    spaces')
         self.assertEqual(first_line['spaces_at_end_only'], 'spaces')
@@ -69,20 +41,20 @@ class Test_can_read_a_spreadsheet(Test_Do_Setups):
         self.assertEqual(second_line['5 as float'], None)
 
     def test_bad_header_sheet(self):
-        data_generator = file_utilities.spreadsheet_keyvalue_generator(test_bad_header_spreadsheet_path)
+        data_generator = file_utilities.spreadsheet_keyvalue_generator(common.test_bad_header_spreadsheet_path)
         with self.assertRaises(file_utilities.InputSpreadsheetException) as context:
             data_generator.__next__()
         self.assertIn('has at least one empty header column', context.exception.reasons[0])
 
     def test_header_only_sheet(self):
-        data_generator = file_utilities.spreadsheet_keyvalue_generator(test_header_only_spreadsheet_path)
+        data_generator = file_utilities.spreadsheet_keyvalue_generator(common.test_header_only_spreadsheet_path)
         with self.assertRaises(file_utilities.InputSpreadsheetException) as context:
             data_generator.__next__()
         self.assertIn('spreadsheet is only headers', context.exception.reasons[0])
 
     def test_empty_spreadsheet_sheet(self):
         with self.assertRaises(file_utilities.InputSpreadsheetException) as context:
-            data_generator = file_utilities.spreadsheet_keyvalue_generator(test_empty_spreadsheet_path)
+            data_generator = file_utilities.spreadsheet_keyvalue_generator(common.test_empty_spreadsheet_path)
             data_generator.__next__()
         self.assertIn('spreadsheet is empty', context.exception.reasons[0])
 
@@ -90,9 +62,9 @@ class Test_can_read_a_spreadsheet(Test_Do_Setups):
 class TestWritingToAn_xlsx(Test_Do_Setups):
     def setUp(self):
         super().setUp()
-        self.test_directory = setup_common_for_test.read_test_locations()
+        self.test_directory = common.read_test_locations()
         self.data_generator = file_utilities.spreadsheet_keyvalue_generator(self.test_directory['person_table_example'])
-        self.test_write_path = os.path.join(test_source_path, 'TEST_WRITE.xlsx')
+        self.test_write_path = os.path.join(common.test_source_path, 'TEST_WRITE.xlsx')
 
     def tearDown(self):
         super().tearDown()
