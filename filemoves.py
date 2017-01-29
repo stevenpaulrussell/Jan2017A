320b2b0e2866d59dd2400afc5435bd7d916b800e
steve <steven_russell@hotmail.com>
2017-01-29 09:53:30 -0800

    Have working filter to find imports by matching with sets

--- filemoves.py
+++ filemoves.py
@@ -1,6 +1,13 @@
 import os
 import shutil
 
+import file_utilities
 
-def move_alia_to_path(alias, locator, path):
+
+def move_alias_to_path(alias, locator, path):
     pass
+
+
+def find_imports_directories_matching_rules(locator_path, **matchlist):
+    imports_gen = file_utilities.spreadsheet_keyvalue_generator(locator_path)
+    return  [line for line in imports_gen if set(matchlist.items()).issubset(line.items())]
--- test_filemoves.py
+++ test_filemoves.py
@@ -1,4 +1,5 @@
 import unittest
+import os.path
 
 import setup_common_for_test
 import filemoves
@@ -9,8 +10,17 @@ imports_path = test_directory['imports_locator']
 
 
 class MoveCopyAndDeleteUsingLocators(unittest.TestCase):
-    def test_can_move_test_files_by_alias_to_imports_by_path(self):
-        result = filemoves.move_alia_to_path('person_table_example', test_directory, imports_path)
+    def test_can_get_import_paths_by_match(self):
+        to_match = dict(table='person', action='import whole', system='steve air')
+        result = filemoves.find_imports_directories_matching_rules(imports_path, **to_match)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0]['table'], 'person')
+        self.assertTrue(os.path.exists(result[0]['path']))
+        print(result[0]['path'])
+
+
+    def xtest_can_move_test_files_by_alias_to_imports_by_path(self):
+        result = filemoves.move_alias_to_path('person_table_example', test_directory, imports_path)
         self.assertEqual(True, result)
 
 
