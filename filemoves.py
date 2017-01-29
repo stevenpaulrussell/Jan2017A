0c2573f394e2dc88013563de8492ca781c72d916
steve <steven_russell@hotmail.com>
2017-01-29 11:47:13 -0800

    Intermediate commit.  Copying ok with error checking.  Tests incomplete.  May want more functions.

--- filemoves.py
+++ filemoves.py
@@ -3,11 +3,32 @@ import shutil
 
 import file_utilities
 
+class FileMovesSurprise(Exception):
+    """Flag any unusual cases"""
 
-def move_alias_to_path(alias, locator, path):
-    pass
 
+def copy_alias_to_path(alias, locator, path_for_copy):
+    source_path = locator[alias]
+    source_file_name = os.path.split(source_path)[-1]
+    copied_path = os.path.join(path_for_copy, source_file_name)
+    if not os.path.isdir(path_for_copy):
+        msg = '{} not a directory, is target for {}'.format(path_for_copy, source_path)
+        raise FileMovesSurprise(msg)
+    if os.path.exists(copied_path):
+        msg = 'Found {} already has {}'.format(path_for_copy, source_file_name)
+        raise FileMovesSurprise(msg)
+    else:
+        shutil.copyfile(source_path, copied_path)
 
-def find_imports_directories_matching_rules(locator_path, **matchlist):
+def find_unique_import_directory_matching_pattern(locator_path, **matchlist):
+    matches = find_all_imports_lines_matching_pattern(locator_path, **matchlist)
+    if len(matches) != 1:
+        msg = 'Found {}, expected exactly 1 match for {}'.format(len(matches), **matchlist)
+        raise FileMovesSurprise(msg)
+    else:
+        return matches[0]['path']
+
+def find_all_imports_lines_matching_pattern(locator_path, **matchlist):
     imports_gen = file_utilities.spreadsheet_keyvalue_generator(locator_path)
     return  [line for line in imports_gen if set(matchlist.items()).issubset(line.items())]
+
--- test_filemoves.py
+++ test_filemoves.py
@@ -12,16 +12,21 @@ imports_path = test_directory['imports_locator']
 class MoveCopyAndDeleteUsingLocators(unittest.TestCase):
     def test_can_get_import_paths_by_match(self):
         to_match = dict(table='person', action='import whole', system='steve air')
-        result = filemoves.find_imports_directories_matching_rules(imports_path, **to_match)
+        result = filemoves.find_all_imports_lines_matching_pattern(imports_path, **to_match)
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]['table'], 'person')
         self.assertTrue(os.path.exists(result[0]['path']))
-        print(result[0]['path'])
 
+    def test_can_get_unique_import_path_by_match(self):
+        to_match = dict(table='person', action='import whole', system='steve air')
+        path = filemoves.find_unique_import_directory_matching_pattern(imports_path, **to_match)
+        self.assertTrue(os.path.exists(path))
 
-    def xtest_can_move_test_files_by_alias_to_imports_by_path(self):
-        result = filemoves.move_alias_to_path('person_table_example', test_directory, imports_path)
-        self.assertEqual(True, result)
+    def test_copy_alias_to_path(self):
+        to_match = dict(table='person', action='import whole', system='steve air')
+        copy_path = filemoves.find_unique_import_directory_matching_pattern(imports_path, **to_match)
+        result = filemoves.copy_alias_to_path('person_table_example', test_directory, copy_path)
+
 
 
 if __name__ == '__main__':
