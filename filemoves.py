a9c8da29d1e233a348156dc06046e5992fd8796f
steve <steven_russell@hotmail.com>
2017-01-29 15:42:16 -0800

    Moves coded, tested for working but not for exceptions.

--- filemoves.py
+++ filemoves.py
@@ -7,22 +7,39 @@ class FileMovesSurprise(Exception):
     """Flag any unusual cases"""
 
 
+def copy_file_path_to_alias_named_directory(source_file_path, alias, locator):
+    """Copy specified file to a directory named by alias in locator.  Use for test and moving imports after done."""
+    dest_path = locator[alias]
+    dest_dir, file_name_must_be_star = os.path.split(dest_path)
+    if file_name_must_be_star != '*':
+        msg = 'alias {} shows path {} filename "{}" not "*"'.format(alias, dest_path, file_name_must_be_star)
+        raise FileMovesSurprise(msg)
+    return _copy_file_path_to_dir(source_file_path, dest_dir)
+
+
 def copy_alias_to_path(alias, locator, path_for_copy):
+    """Copy a file named in a 'locator' spreadsheet to a directory specified by path_for_copy. Return copied to path"""
     source_path = locator[alias]
-    source_file_name = os.path.split(source_path)[-1]
-    copied_path = os.path.join(path_for_copy, source_file_name)
-    if not os.path.isdir(path_for_copy):
-        msg = '{} not a directory, is target for {}'.format(path_for_copy, source_path)
+    return _copy_file_path_to_dir(source_path, path_for_copy)
+
+
+def _copy_file_path_to_dir(source_file_path, dir_path):
+    """Common pattern used for alias and import copying """
+    source_file_name = os.path.split(source_file_path)[-1]
+    copied_path = os.path.join(dir_path, source_file_name)
+    if not os.path.isdir(dir_path):
+        msg = '{} not a directory, is target for {}'.format(dir_path, source_file_path)
         raise FileMovesSurprise(msg)
     if os.path.exists(copied_path):
-        msg = 'Found {} already has {}'.format(path_for_copy, source_file_name)
+        msg = 'Found {} already has {}'.format(dir_path, source_file_name)
         raise FileMovesSurprise(msg)
     else:
-        shutil.copyfile(source_path, copied_path)
+        shutil.copyfile(source_file_path, copied_path)
     return copied_path
 
 
 def find_unique_import_directory_matching_pattern(locator_path, **matchlist):
+    """Returns path of unique import directory, or raises exception if unique not found."""
     matches = find_all_imports_lines_matching_pattern(locator_path, **matchlist)
     if len(matches) != 1:
         msg = 'Found {}, expected exactly 1 match for {}'.format(len(matches), **matchlist)
@@ -32,6 +49,7 @@ def find_unique_import_directory_matching_pattern(locator_path, **matchlist):
 
 
 def find_all_imports_lines_matching_pattern(locator_path, **matchlist):
+    """Pattern matching aka sql select. Mostly for test, find import directories with wanted properties"""
     imports_gen = file_utilities.spreadsheet_keyvalue_generator(locator_path)
     return  [line for line in imports_gen if set(matchlist.items()).issubset(line.items())]
 
--- test_filemoves.py
+++ test_filemoves.py
@@ -1,5 +1,6 @@
 import unittest
-import os.path
+import os
+import shutil
 
 import setup_common_for_test
 import filemoves
@@ -10,6 +11,13 @@ imports_path = test_directory['imports_locator']
 
 
 class MoveCopyAndDeleteUsingLocators(unittest.TestCase):
+    def setUp(self):
+        self.file_to_get = []
+
+    def tearDown(self):
+        for a_file_path in self.file_to_get:
+            os.remove(a_file_path)
+
     def test_can_get_import_paths_by_match(self):
         to_match = dict(table='person', action='import whole', system='steve air')
         result = filemoves.find_all_imports_lines_matching_pattern(imports_path, **to_match)
@@ -22,14 +30,28 @@ class MoveCopyAndDeleteUsingLocators(unittest.TestCase):
         path = filemoves.find_unique_import_directory_matching_pattern(imports_path, **to_match)
         self.assertTrue(os.path.exists(path))
 
-    def test_copy_alias_to_path(self):
+    def test_copy_alias_to_import_path_by_match_person_whole(self):
         to_match = dict(table='person', action='import whole', system='steve air')
         copy_path = filemoves.find_unique_import_directory_matching_pattern(imports_path, **to_match)
-        copied_file_path = filemoves.copy_alias_to_path('person_table_example', test_directory, copy_path)
-        self.assertTrue(os.path.exists(copied_file_path))
-        self.assertEqual(os.path.split(copied_file_path)[0], copy_path)
-        print(copied_file_path)
+        copied_path = filemoves.copy_alias_to_path('person_table_example', test_directory, copy_path)
+        self.file_to_get.append(copied_path)
+        self.assertTrue(os.path.exists(copied_path))
+        self.assertEqual(os.path.split(copied_path)[0], copy_path)
 
+    def test_copy_file_path_to_dir_named_by_alias(self):
+        """Takes file at copy_path_and_moves_to_alias_iff_alias_is_directory"""
+        a_file_path = test_directory['table_as_query_cmds']
+        print(a_file_path)
+        copied_path = filemoves.copy_file_path_to_alias_named_directory(a_file_path, 'dropbox_test', test_directory)
+        self.file_to_get.append(copied_path)
+        source_name = os.path.split(a_file_path)[-1]
+        copy_name = os.path.split(copied_path)[-1]
+        self.assertEqual(source_name, copy_name)
+        self.assertTrue(os.path.exists(copied_path))
+
+
+    def test_remove_file_at_path(self):
+        """Maybe have a trash store, and this copies to trash with time stamp then removes?"""
 
 
 if __name__ == '__main__':
