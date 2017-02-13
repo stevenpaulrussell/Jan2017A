d9ce7121171b1f08dbd2126a56e087fa536860ea
steve <steven_russell@hotmail.com>
2017-02-13 11:15:03 -0800

    Functional tests now seem to be passing except for note on what still needs doing

--- test_functional.py
+++ test_functional.py
@@ -108,14 +108,16 @@ class InsertFromImportsIncrementally(unittest.TestCase):
     def tearDown(self):
         setup_common_for_test.clean_directories()
 
-    def xtest_incremental_with_errors(self):
-        import_directory_path = file_utilities.get_path_from_alias('person_draft_error')
-        import_name = os.path.split(import_path)[-1]
-        dest_path = file_utilities.get_path_from_alias('import lines person directory')
+    def test_incremental_with_errors(self):
+        test_file_path = file_utilities.get_path_from_alias('person_draft_error')
+        test_file_name = os.path.split(test_file_path)[-1]
+        destination_directory = file_utilities.get_path_from_alias('import lines person directory')
+        imported_file_path  = os.path.join(destination_directory, test_file_name)
         archive_directory = file_utilities.get_path_from_alias('archive_directory/person')
-        archive_path = os.path.join(archive_directory, import_name)
+        archive_path = os.path.join(archive_directory, test_file_name)
 
-        file_utilities.copy_file_path_to_dir(import_path, dest_path)
+        file_utilities.copy_file_path_to_dir(test_file_path, destination_directory)
+        self.assertTrue(os.path.exists(imported_file_path))  # import file remains but is changed
         success, history = action.do_a_work_item(connect=connect)
         (first_command, vars), first_psycop_response = history[0]
         value_mapping_as_dict = vars[0]
@@ -123,13 +125,14 @@ class InsertFromImportsIncrementally(unittest.TestCase):
         self.assertFalse(success)
         self.assertIn('INSERT INTO', first_command)
         self.assertTrue(value_mapping_as_dict.keys())
-        self.assertTrue(os.path.exists(import_path))  # import file remains but is changed
+        self.assertIn("""IntegrityError('null value in column "last" violates not-null constraint""",
+                      first_psycop_response)
+        self.assertTrue(os.path.exists(imported_file_path))  # import file remains but is changed
         self.assertTrue(os.path.exists(archive_path))  # successful import copied here for safetyZ
 
 
-    def xtest_work_to_do(self):
+    def test_work_to_do(self):
         self.assertFalse('Line_at_a_time has no problem when the whole file is clean and imports all lines')
-        self.assertFalse('test_action cleans up after itself, tests various end cases')
 
 
 
